2ad92acfd5488f8032056b496f457ce6adab3886
Christian Urtubia Rodriguez <christian.urodriguez@gmail.com>
2022-08-05 23:39:40 -0400

    commit

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Hola {{ nombre }}</h1>
+    <button class="btn btn-primary">hola</button>
+</template>
+
+<script lang="ts" setup>
+    import { ref } from 'vue';
+    const nombre = ref<string>('Hola gato');
+
+</script>
